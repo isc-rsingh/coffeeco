64d78a7d58a66ff9cdde5fb1cc2f655dcccca833
isc-rsingh <rajrsingh@gmail.com>
2021-03-19 01:00:18 -0400

    better about content

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="about">
-    <h2>Learn the InterSystems IRIS Data Platform</h2>
-    <ul>
-      <li>Getting started tutorial</li>
-      <li>Code for Part 1: Database setup</li>
-      <li>Code for Part 2: Web services</li>
-      <li>Code for Part 3: Front end single-page app with Vue.js</li>
-    </ul>
+    <h2>Full Stack Tutorial for the InterSystems IRIS Data Platform</h2>
+    <p>In this tutorial, you create the basic information management infrastructure for a small manufacturing company. In this case, the company roasts, packages and sells delicious, freshly roasted coffee beans. Along the way, you learn how the InterSystems IRIS data platform can serve as the backbone of your IT architecture.</p>
+
+    <p>It’s divided into three parts:
+    <ol>
+      <li><a href="https://gettingstarted.intersystems.com/full-stack/full-stack-part-one/" target="_new">part 1</a>: create database tables and write raw coffee bean deliveries to the database.</li>
+      <li><a href="https://gettingstarted.intersystems.com/full-stack/part-two-rest-services/" target="_new">part 2</a>: build web services that power the roasting operations and online store.</li>
+      <li><a href="https://gettingstarted.intersystems.com/full-stack/part-three-front-end/" target="_new">part 3</a>: build a web storefront in Vue.js (results in this web app!)</li>
+    </ol>
+    </p>
+
+    <p><a href="https://github.com/intersystems/coffeeco" target="_new">Here is the GitHub repository with all the code</a></p>
   </div>
 </template>
