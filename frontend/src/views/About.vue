db2170ad0bccd8e030a0aefb5e73a4431b60305d
Anson Zhong <azhong@intersystems.com>
2025-06-18 14:01:15 -0400

    fix up HTML warning

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -6,13 +6,12 @@
  packages and sells delicious, freshly roasted coffee beans. Along the way, you learn how
  the InterSystems IRIS data platform can serve as the backbone of your IT architecture.</p>
 
-    <p>It&apos;s divided into three parts:
+    <p>It&apos;s divided into three parts:</p>
     <ol>
       <li><a href="https://gettingstarted.intersystems.com/full-stack/full-stack-part-one/" target="_new">part 1</a>: create database tables and write raw coffee bean deliveries to the database.</li>
       <li><a href="https://gettingstarted.intersystems.com/full-stack/part-two-rest-services/" target="_new">part 2</a>: build web services that power the roasting operations and online store.</li>
       <li><a href="https://gettingstarted.intersystems.com/full-stack/part-three-front-end/" target="_new">part 3</a>: build a web storefront in Vue.js (results in this web app!)</li>
     </ol>
-    </p>
 
     <p><a href="https://github.com/isc-rsingh/coffeeco" target="_new">Here is the GitHub repository with all the code</a></p>
   </div>
